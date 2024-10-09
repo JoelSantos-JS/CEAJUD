701c444902eff11b5371a845492cf9fe44edfb59
unknown <joeltere9@gmail.com>
2024-10-09 17:00:43 -0300

    first commit

---
+++ src/test/java/com/joel/br/Java/A/JavaAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joel.br.Java.A;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
